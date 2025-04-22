7bbfcd9da4c2d5d65189e9f83a864f1a7beb463d
jake-bisho <17jbisho@gmail.com>
2025-04-22 19:48:37 +1000

    done

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
     book = get_book_text(filepath)
     word_count = count_words(book)
     freq_dict = char_freq(book)
